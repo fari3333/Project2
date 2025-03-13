53d6bcc8ac9ab09b1d20b12bb3e99a10c1edca17
Fari <adepf459@newschool.edu>
2025-03-13 15:44:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,43 @@
-// const fontImages = {};
-// const imageFolder = "./images/";
-// const invalidInputGif = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmtkdnJhMXY4Njd1cXR3MTM3MW1nMzZ1NXNhNHlhOHA0MWF2dDUzdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13dRJkj5wgKq9q/giphy.gif";
-
-// Generate font image paths for A-Z and numbers 1-9
-// for (let i = 65; i <= 90; i++) { 
-//     const letter = String.fromCharCode(i);  
-//     for (let num = 1; num <= 9; num++) { 
-//         fontImages[`${letter}${num}`] = `${imageFolder}${letter}${num}.png`;
-//     }
-// }
-
 const fontImages = {};
-const imageFolder = "./images/"; 
+const imageFolder = "./images/";
 const invalidInputGif = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmtkdnJhMXY4Njd1cXR3MTM3MW1nMzZ1NXNhNHlhOHA0MWF2dDUzdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13dRJkj5wgKq9q/giphy.gif";
 
 
-for (let i = 65; i <= 90; i++) { 
-    const letter = String.fromCharCode(i); 
-    fontImages[letter] = `${imageFolder}${letter}.png`; 
+for (let i = 65; i <= 90; i++) {
+    const letter = String.fromCharCode(i);
+    for (let num = 1; num <= 6; num++) { 
+        fontImages[`${letter}${num}`] = `${imageFolder}${letter}${num}.png`;
+    }
 }
-function displayImage() {
-    const input = document.getElementById("charInput").value.toUpperCase();
+
+function updateFontDisplay() {
+    const letterInput = document.getElementById("charInput").value.toUpperCase();
+    const sliderValue = document.getElementById("type-slider").value;
     const imgElement = document.getElementById("displayedImage");
+    const fontKey = letterInput + sliderValue;
+    
+    if (/^[A-Z]$/.test(letterInput)) {
+        let filePath = fontImages[fontKey] || invalidInputGif;
 
-    if (/^[A-Z]$/.test(input)) {
         let img = new Image();
-        img.src = fontImages[input];
-
+        img.src = filePath;
         img.onload = function () {
-            imgElement.src = fontImages[input];
+            imgElement.src = filePath;
             imgElement.style.display = "block";
         };
-    img.onerror = function () {
-        imgElement.src = invalidInputGif; 
+        img.onerror = function () {
+            imgElement.src = invalidInputGif;
+            imgElement.style.display = "block";
+        };
+
+        document.getElementById("typeface-name").textContent = fontCategories[sliderValue];
+        document.getElementById("typeface-name").style.visibility = "visible";
+    } else {
+        imgElement.src = invalidInputGif;
         imgElement.style.display = "block";
-    };
-} else {
-    imgElement.src = invalidInputGif; 
-    imgElement.style.display = "block";
-}
+    }
 }
 
-// function displayImage() {
-//     const letterInput = document.getElementById("charInput").value.toUpperCase();
-//     const numberInput = document.getElementById("numInput").value;
-//     const imgElement = document.getElementById("displayedImage");
-
-//     const fontKey = letterInput + numberInput;
-
-//     if (/^[A-Z]$/.test(letterInput) && /^[1-9]$/.test(numberInput)) {
-//         let filePath = fontImages[fontKey];
-
-//         let img = new Image();
-//         img.src = filePath;
-//         img.onload = function () {
-//             imgElement.src = filePath;
-//             imgElement.style.display = "block";
-//         };
-//         img.onerror = function () {
-//             imgElement.src = invalidInputGif;
-//             imgElement.style.display = "block";
-//         };
-//     } else {
-//         imgElement.src = invalidInputGif;
-//         imgElement.style.display = "block";
-//     }
-// }
 
 document.addEventListener("DOMContentLoaded", function () {
     const popup = document.getElementById("popup-card");
@@ -73,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     popup.addEventListener("click", function () {
         popup.style.transform = "scale(1.1)"; 
-        popup.style.boxShadow = "10px 10px 0px black"; 
+        popup.style.boxShadow = "10px 10px 10px black"; 
 
         setTimeout(() => {
             popup.style.opacity = "0";
@@ -86,19 +58,40 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(() => popup.style.display = "none", 300);
     });
 });
+document.getElementById("submitButton").addEventListener("click", function () {
+    this.style.transform = "scale(0.9)";
+    setTimeout(() => {
+        this.style.transform = "scale(1)";
+    }, 150);
+});
+const submitButton = document.getElementById('submitButton');
+const displayedImage = document.getElementById('displayedImage');
+
+submitButton.addEventListener('click', function() {
+    displayedImage.classList.remove('show', 'shadow-effect');
+
+    
+    void displayedImage.offsetWidth; 
+    displayedImage.classList.add('show');
+
+   
+    setTimeout(() => {
+        displayedImage.classList.add('shadow-effect');
+    }, 300); 
+});
+
+
 const typeSlider = document.getElementById("type-slider");
 const typefaceName = document.getElementById("typeface-name");
 
 const typefaces = {
     1: "Serif",
     2: "Sans Serif",
-    3: "Slab Serif",
+    3: "Decorative",
     4: "Monospace",
     5: "Script",
     6: "Display",
-    7: "Handwriting",
-    8: "Gothic",
-    9: "Decorative"
+
 };
 
 typeSlider.addEventListener("input", function() {
